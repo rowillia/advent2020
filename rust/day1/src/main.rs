3ecd174f7c8179eaa7e3ecba679bb3a5c459f8b9
Roy Williams <rwilliams@lyft.com>
2020-12-02 14:01:13 -0500

    Format rust day 1

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -3,8 +3,7 @@ use std::env;
 use std::io;
 use std::io::BufRead;
 
-fn two_sum(values: impl Iterator<Item=i32>, target: i32) -> Option<(i32, i32)>
-{
+fn two_sum(values: impl Iterator<Item = i32>, target: i32) -> Option<(i32, i32)> {
     let mut values_as_set = HashSet::<i32>::new();
     for value in values {
         if values_as_set.contains(&(target - value)) {
@@ -13,37 +12,40 @@ fn two_sum(values: impl Iterator<Item=i32>, target: i32) -> Option<(i32, i32)>
             values_as_set.insert(value);
         }
     }
-    return None;
+    None
 }
 
-fn three_sum<'a>(values: Vec<i32>, target: i32) -> Option<(i32, i32, i32)>
-{
+fn three_sum(values: &[i32], target: i32) -> Option<(i32, i32, i32)> {
     for (i, value) in values.iter().enumerate() {
-        match two_sum(values.iter().enumerate().filter(|&(j, _)| i != j).map(|(_, v)| *v), target - value) {
-            Some(n) => return Some((n.0, n.1, *value)),
-            None => ()
+        if let Some(n) = two_sum(
+            values
+                .iter()
+                .enumerate()
+                .filter(|&(j, _)| i != j)
+                .map(|(_, v)| *v),
+            target - value,
+        ) {
+            return Some((n.0, n.1, *value));
         }
     }
-    return None;
+    None
 }
 
 fn main() {
     let stdin = io::stdin();
-    let input = stdin.lock().lines().map(|l| l.unwrap().parse::<i32>().unwrap());
+    let input = stdin
+        .lock()
+        .lines()
+        .map(|l| l.unwrap().parse::<i32>().unwrap());
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 && args[1] == "-3" {
-        match three_sum(input.collect(), 2020) {
-            Some(n) => println!("{:?}", n.0 * n.1 * n.2),
-            None => ()
+        if let Some(n) = three_sum(input.collect(), 2020) {
+            println!("{:?}", n.0 * n.1 * n.2)
         }
-    } else {
-        match two_sum(input, 2020) {
-            Some(n) => println!("{:?}", n.0 * n.1),
-            None => ()
-        } 
+    } else if let Some(n) = two_sum(input, 2020) {
+        println!("{:?}", n.0 * n.1)
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -56,12 +58,12 @@ mod tests {
         assert_eq!(two_sum([1, 2, 3].iter().cloned(), 6), None);
         Ok(())
     }
-    
+
     #[test]
     fn test_three_sum() -> Result<(), String> {
-        assert_eq!(three_sum(vec![1, 2, 3], 4), None);
-        assert_eq!(three_sum(vec![1, 2, 3], 42), None);
-        assert_eq!(three_sum(vec![1, 2, 3], 6), Some((3, 2, 1)));
+        assert_eq!(three_sum(&[1, 2, 3], 4), None);
+        assert_eq!(three_sum(&[1, 2, 3], 42), None);
+        assert_eq!(three_sum(&[1, 2, 3], 6), Some((3, 2, 1)));
         Ok(())
     }
 }
